b18e54200e9d6764bd7b6ffdc2300a2b5de076cd
clstialdev <rusty75001@gmail.com>
2026-01-07 02:13:11 +0100

    feat: new ui

--- apps/frontend/providers/query-provider.tsx
+++ apps/frontend/providers/query-provider.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 import { ReactNode } from "react";
 
 interface QueryProviderProps {
   children: ReactNode;
 }
 
-const queryClient = new QueryClient();
 function QueryProvider({ children }: QueryProviderProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
